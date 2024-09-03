14a86d5f6960d51dc38f25f77ab09f51c2675381
Samgrass <samuelegrassetti01@gmail.com>
2024-09-03 17:31:12 +0200

    feat: add year required to Movie

--- Model/movie.php
+++ Model/movie.php
@@ -3,9 +3,11 @@
 class Movie
 {
     public $name;
+    public $year;
 
-    function __construct($_name)
+    function __construct($_name, $_year)
     {
         $this->name = $_name;
+        $this->year = $_year;
     }
 }
