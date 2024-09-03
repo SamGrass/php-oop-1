b693a806c593192fe82fa3068186a30d8cda4adf
Samgrass <samuelegrassetti01@gmail.com>
2024-09-03 17:31:41 +0200

    feat: add some movies to the db

--- db/db-movies.php
+++ db/db-movies.php
@@ -2,5 +2,8 @@
 require_once __DIR__ . '/../Model/movie.php';
 
 $movies = [
-    new Movie('Star Wars')
+    new Movie('Star Wars IV', 1977),
+    new Movie('Il Padrino', 1972),
+    new Movie('Inception', 2010),
+    new Movie('Titanic', 1997)
 ];
